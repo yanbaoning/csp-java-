4fb9d23073706413d02fed1e06f97e475b8337fc
yanbaoning <37328978+yanbaoning@users.noreply.github.com>
2019-09-08 15:43:09 +0800

    小明上学
    2018-12-01

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package test01;
+package test02;
 
 import java.util.Scanner;
 
@@ -26,10 +26,13 @@ public class Main {
 		Scanner sc=new Scanner(System.in);
 		//定义总时间
 		int sumtime=0;
+		//同步时间
+		int currenttime=0;
 		//正整数 r、y、g，表示红绿灯的设置
 		int r = sc.nextInt();
 		int y = sc.nextInt();
 		int g = sc.nextInt();
+		int T=r+y+g;
 		//n表示小明总共经过的道路段数和看到的红绿灯数目
 		int n = sc.nextInt();
 		//k=0 表示经过了一段道路，耗时 t 秒
@@ -38,18 +41,34 @@ public class Main {
 		for(int i=1;i<=n;i++) {
 			int k = sc.nextInt();
 			int t = sc.nextInt();
+			currenttime=sumtime%T;
 			switch(k) {
 			case 0:		//0表示走了t秒路
 				sumtime+=road(t);
 				break;
 			case 1:		//1表示红灯t秒
-				sumtime+=red(t);
+				if(currenttime>t && currenttime<t+g) {
+					
+				}else if(currenttime<t) {
+					sumtime+=t-sumtime;
+				}else if(currenttime>=t+g) {
+					sumtime+=t+g+y+r-currenttime;
+				}
 				break;
 			case 2:		//2表示黄灯t秒
-				sumtime+=yellow(t,r);
+				if(currenttime>=t+r&&currenttime<t+r+g) {
+					
+				}else if(currenttime<t+r) {
+					sumtime+=t+r-currenttime;
+				}else if(currenttime>=t+r+g) {
+					sumtime+=t+r+g+y+r-currenttime;
+				}
 				break;
 			case 3:		//3表示绿灯t秒
-				sumtime+=green();
+				//sumtime+=green();
+				if(sumtime>=t && sumtime<t+y+r) {
+					sumtime+=t+y+r-sumtime;
+				}
 				break;
 			}
 		}
